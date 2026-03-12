20152916167fd8a215be5ab85a9bf630fd6325db
DeepByte1 <maximmaksunov@gmail.com>
2026-03-12 11:23:34 +0300

    Update 01_Hello_OLED_Minima.ino
    Translated notes to english

--- examples/01_Hello_OLED_Minima/01_Hello_OLED_Minima.ino
+++ examples/01_Hello_OLED_Minima/01_Hello_OLED_Minima.ino
@@ -1,22 +1,21 @@
 #include <OLED_Minima.h>
 
-OLED_Minima oled; // по умолчанию адрес 0x3C
+OLED_Minima oled; // by default adress 0x3C
 
 
 void setup() {
-  // Параметры: (Модель, Отразить_X, Отразить_Y)
-  // Если экран выводит страшные символы, попробуйте поменять первое значение (Версия) туда сюда. (0=Оригинал/Качественная, 1=Китайская, 2=Другая Китайская)
-  // Также пользуётесь отражением по X и Y, исходя из того как расположен дисплей
-  // можно не давать агрументы, тогда будет 0, false, false
+  // Parmeters: (Model, Mirror_X, Mirrir_Y)
+  // if the screen is going insane, try changing the first value (model). (0=Original, 1=Copy, 2=Other Copy)
+  // also dont forget to use mirror depending on how is screen located!
   oled.begin(0, true, false); 
-  oled.DISPLAY_HEIGHT = 64; // высота экрана (в пикселях). по умолчанию 64 (8 строк)
-  oled.CHAR_SPACING = true; // пробел 1 пиксель между буквами. по умолчанию true
+  oled.DISPLAY_HEIGHT = 64; // screen height (in pixels). by default 64 (8 pages)
+  oled.CHAR_SPACING = true; // 1 pixel space between letters. by default true
   
-  oled.setCursor(13, 1); // X это пиклеси 0-127. Y это строка по 8 пикселей. при высоте в 64 пикселя, это 0-7.
-  oled.println("HELLO!"); // println() выведет данные, а потом перейдёт на новую строку
-  oled.print("VAL: "); // print() просто выведет данные, позволяя...
-  oled.println(100); // ...продолжить писать с того же места
-  oled.println(F("Flash-string")); // поддержка разных типов, включая строки("Hello"), отдельные буквы('A'), цифры (123), Flash-строки(F("Hello"))
+  oled.setCursor(13, 1); // X are pixels lengh. With screen width of 128, its 0-127 (not 1-128!). Y is a page 8 pixels each. with screen size of 64 pages are 0-7.
+  oled.println("HELLO!"); // println() will print the data, and then go to the new line.
+  oled.print("VAL: "); // print() will simply print the data...
+  oled.println(100); // ...allowing to continue from the same place
+  oled.println(F("Flash-string")); // supports different types including: strings ("Hello"), characters ('A'), numbers (123), Flash-strings (F("Hello")), and also an empty println() to simply go to the next line.
 }
 
 void loop() {}
